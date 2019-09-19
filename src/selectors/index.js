6644c4b173ffd348c60e6316aedd639f71eca835
swonb <swonbinca@gmail.com>
2019-09-19 17:26:58 -0400

    Added (memoized) Selector function

---
+++ src/selectors/index.js
@@ -0,0 +1,26 @@
+import { createSelector } from "reselect";
+
+const getVisibilityFilter = state => state.visibilityFilter;
+const getTodos = state => state.todos;
+
+export const getVisibleTodos = createSelector(
+  [getVisibilityFilter, getTodos],
+  (visibilityFilter, todos) => {
+    switch (visibilityFilter) {
+      case "SHOW_ALL":
+        return todos;
+      case "SHOW_COMPLETED":
+        return todos.filter(t => t.completed);
+      case "SHOW_ACTIVE":
+        return todos.filter(t => !t.completed);
+    }
+  }
+);
+
+const getKeyword = state => state.keyword;
+
+const getVisibleTodosFilteredByKeyword = createSelector(
+  [getVisibleTodos, getKeyword],
+  (visibleTodos, keyword) =>
+    visibleTodos.filter(todo => todo.text.indexOf(keyword) > -1)
+);
